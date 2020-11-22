a770e336d59bafd8894b105b8a15b1bf36b6850d
MrSanderSi <sihversass@gmail.com>
2020-11-22 22:27:21 +0200

    a little login update

--- homework3/src/App.vue
+++ homework3/src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/login.vue'
 
 export default {
   name: 'App',
---
+++ homework3/src/components/login.vue
@@ -0,0 +1,72 @@
+<template>
+  <head>
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="res/css/style.css">
+  <title>Welcome to postIt! Please Log In</title>
+</head>
+<body>
+    <section class="login-page">
+      <section id="login-container">
+        <div>
+          <h1>Welcome to postIt!</h1>
+        </div>
+        <div>
+          <h4>
+            <a href="#">Create an Account</a>
+          </h4>
+        </div>
+        <div>
+          <h4>Or</h4>
+        </div>
+        <div>
+          <h3>Please Log In</h3>
+          <form action="index.html" method="post">
+            <div>
+              <input type="text" name="email" placeholder="Email">
+            </div>
+            <div>
+              <input type="password" name="password" placeholder="Password">
+            </div>
+            <div>
+              <button class="button" type="submit" name="login">Log In</button>
+            </div>
+            <div>
+              <small>
+                <a href="#">Forgot password?</a>
+              </small>
+            </div>
+          </form>
+        </div>
+      </section>
+    </section>
+  </body>
+
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
